e565c7925631006ae45747c9655e80a416866793
piyixia18 <81619027+piyixia18@users.noreply.github.com>
2022-06-05 12:00:19 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://11.ixxtube.com/', "_self");
+            window.open('https://11.jinlianys.com/dabo/shouye.html', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://12.ixxtube.com/', "_self");
+            window.open('https://12.jinlianys.com/dabo/shouye.html', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://13.ixxtube.com/', "_self");
+            window.open('https://13.jinlianys.com/dabo/shouye.html', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://16.ixxtube.com/', "_self");
+            window.open('https://16.jinlianys.com/dabo/shouye.html', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.baidu.com', "_blank");
