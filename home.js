de0113db613ca1892b2a6d87286f071fdf9bf999
piyixia18 <81619027+piyixia18@users.noreply.github.com>
2022-06-09 20:55:18 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://www.bigbo1.cc/dabo/shouye.html', "_self");
+            window.open('https://www.bigbo1.cc', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://www.bigbo1.cc/dabo/shouye.html', "_self");
+            window.open('https://www.bigbo2.cc', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://www.bigbo1.cc/dabo/shouye.html', "_self");
+            window.open('https://www.bigbo3.cc', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://16.love-words.com/dabo/shouye.html', "_self");
+            window.open('https://66.love-words.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.baidu.com', "_blank");
